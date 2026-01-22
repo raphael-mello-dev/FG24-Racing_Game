241c026eacb90dd0ba67e889d2ff6469b686a4da
Miloet <miloet00@gmail.com>
2026-01-22 13:25:05 +0100

    UI

--- Assets/MainMenu/UI_HoverIcon.cs
+++ Assets/MainMenu/UI_HoverIcon.cs
@@ -1,16 +1,34 @@
+using TMPro.EditorUtilities;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_HoverIcon : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Image image;
+    public Transform showBase;
+    public float speed;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        SetImage(image.sprite);
+    }
     void Update()
     {
-        
+        if(image.sprite != null)
+            showBase.Rotate(Vector3.up, Time.deltaTime * speed, Space.Self);
+    }
+
+    public void SetImage(Sprite sprite)
+    {
+        image.sprite = sprite;
+        if (image.sprite == null)
+        {
+            transform.localScale = Vector3.zero;
+        }
+        else
+        {
+
+            transform.localScale = Vector3.one;
+        }
     }
 }
